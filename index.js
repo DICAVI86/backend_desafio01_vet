348a90427f4d59cbc9d9b0e5c38921739ad6704f
DICAVI86 <delcastillovial@gmail.com>
2024-10-31 17:12:45 -0300

    Se completaron las operaciones, y se ordeno el index

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const {registrar, leer} = require('./operaciones');
 
+const operacion = process.argv[2];
+const nombre = process.argv[3];
+const edad = process.argv[4];
+const especie = process.argv[5];
+const color = process.argv[6];
+const enfermedad = process.argv[7];
 
-const nombre = process.argv[2];
-const edad = process.argv[3];
-const especie = process.argv[4];
-const color = process.argv[5];
-const enfermedad = process.argv[6];
-
+if (operacion === "registrar") {
+    if (enfermedad) {
+      registrar(nombre, edad, especie, color, enfermedad);
+    } else {
+      console.log("Debes ingresar todos los datos del registro");
+    }
+  } else if (operacion === "leer") {
+    leer();
+  } else {
+    console.log("Operacion no reconocida");
+  }
 
--- operaciones.js
+++ operaciones.js
@@ -1,15 +1,49 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const cita = [
-    {
-        nombre: 'nombre',
-        edad: 'edad',
-        especie: 'especie',
-        color: 'color',
-        enfermedad: 'enfermedad'
-    },
-];
+const registrar = (nombre, edad, especie, color, enfermedad) => {
+  fs.readFile("./citas.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error al leer archivo: ", err);
+      return;
+    }
 
-fs.writeFileSync('citas.json', JSON.stringify(cita));
+    const citas = JSON.parse(data);
 
-module.exports = {cita};
+    const nuevaCita = {
+      nombre,
+      edad,
+      especie,
+      color,
+      enfermedad,
+    };
+
+    citas.push(nuevaCita);
+    fs.writeFile("./citas.json", JSON.stringify(citas), (error) => {
+      if (error) {
+        console.error("Error al escribir el archivo: ", err);
+        return;
+      } else {
+        console.log("Cita registrada con Exito");
+      }
+    });
+  });
+};
+
+const leer = () => {
+    fs.readFile("./citas.json", "utf8", (err, data) => {
+        if (err) {
+          console.error("Error al leer archivo: ", err);
+          return;
+        }
+        try {
+          const citas = JSON.parse(data);
+          console.log("Listado de Citas");
+          
+          console.log(citas);
+        } catch (parseError) {
+          console.error("Error al parsear el archivo JSON: ", parseError);
+        }
+      });
+};
+
+module.exports = { registrar, leer };
